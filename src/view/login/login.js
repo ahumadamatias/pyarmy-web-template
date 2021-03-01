adc569e9a97f19d9ac557be9c39a1c3d0fd60a9c
faculuna745 <lunafacundo03@gmail.com>
2021-02-28 23:58:44 -0300

    Add view and logic register

--- src/view/login/login.js
+++ src/view/login/login.js
@@ -1,13 +1,21 @@
-import { TextField } from '@material-ui/core'
+import { FormControl, TextField,Button} from '@material-ui/core'
 import React from 'react'
+
 
 const Login = () => {
     return (
-        <div>
-            <form style={{padding: '24px', display: "flex", flexDirection: "column", width: "70%", margin: 'auto'}}>
-                <TextField variant="outlined" label="Username"/>
-                <TextField type="password" variant="outlined" label="Password"/>
-            </form>
+        <div className="wrapper">
+            <div className="mat-card">
+                <h1>Sign In</h1>
+                <FormControl className ="login-input">
+                    <TextField className="item-card" variant="outlined" label="Username"/>
+                    <TextField className="item-card" type="password" variant="outlined" label="Password"/>
+                </FormControl>
+                <div className="btn-submit">
+                    <Button variant="contained" color="primary">Sign In</Button>
+                </div>
+                
+            </div>
         </div>
     )
 }
